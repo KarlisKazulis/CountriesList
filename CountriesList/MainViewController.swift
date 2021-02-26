e58f047acb71c8f45bbec54800ed559ec4d773b6
KarlisKazulis <karlis.kazulis@gmail.com>
2021-02-26 13:03:48 +0200

    my countriesList app upload

---
+++ CountriesList/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CountriesList
+//
+//  Created by karlis.kazulis on 04/02/2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+    private func setupView(){
+        
+        view.backgroundColor = .white
+        //setupNavigationBar
+    }
+    
+    private func setupNavigationBar(){
+        
+    }
+}
+
